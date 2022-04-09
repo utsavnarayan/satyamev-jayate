a83991212d41b417a8621ce8c96212aee0f624d6
Utsav Narayan Singh <utsavinbox@gmail.com>
2022-04-09 11:50:27 +0530

    Refactor lambda

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,39 +3,6 @@ import Button from "@mui/material/Button";
 import logo from "../static/national-emblem.png";
 import "./Home.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
-
-  handleClick = (api) => (e) => {
-    e.preventDefault();
-
-    this.setState({ loading: true });
-    fetch("/.netlify/functions/" + api)
-      .then((response) => response.json())
-      .then((json) => this.setState({ loading: false, msg: json.msg }));
-  };
-
-  render() {
-    const { loading, msg } = this.state;
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>
-          {loading ? "Loading..." : "Call Lambda"}
-        </button>
-        <button onClick={this.handleClick("async-dadjoke")}>
-          {loading ? "Loading..." : "Call Async Lambda"}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    );
-  }
-}
-
 class Home extends Component {
   render() {
     return (
